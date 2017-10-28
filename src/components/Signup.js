4653a3fd6f434226dd3086f7ecedcd3caf286e50
marlondc <mdatechong@gmail.com>
2017-10-28 17:48:21 +0100

    fix linting errors

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,7 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 
-const Signup = ({ handleMobileNumberChange, handleNameChange, mobileNumber, name }) => (
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontFamily: 'raleway-regular',
+    fontSize: 18,
+    lineHeight: 23,
+    borderWidth: 1,
+    borderColor: '#000',
+    flex: 2,
+  },
+  labelStyle: {
+    flex: 1,
+    fontFamily: 'raleway-regular',
+    fontSize: 18,
+  },
+  fieldStyle: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+};
+
+
+const Signup = ({
+  handleMobileNumberChange,
+  handleNameChange,
+  mobileNumber,
+  name,
+}) => (
   <ScrollView
     scrollEnabled={false}
     contentContainerStyle={styles.containerStyle}
@@ -39,39 +77,6 @@ Signup.propTypes = {
   name: PropTypes.string,
   handleNameChange: PropTypes.func.isRequired,
   handleMobileNumberChange: PropTypes.func.isRequired,
-};
-
-const styles = {
-  inputStyle: {
-    color: '#000',
-    paddingRight: 5,
-    paddingLeft: 5,
-    paddingTop: 5,
-    paddingBottom: 5,
-    fontFamily: 'raleway-regular',
-    fontSize: 18,
-    lineHeight: 23,
-    borderWidth: 1,
-    borderColor: '#000',
-    flex: 2,
-  },
-  labelStyle: {
-    flex: 1,
-    fontFamily: 'raleway-regular',
-    fontSize: 18,
-  },
-  fieldStyle: {
-    alignItems: 'center',
-    // height: 40,
-    flexDirection: 'row',
-  },
-  containerStyle: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
 };
 
 export default Signup;
